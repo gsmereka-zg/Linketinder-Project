38738b0fedcfb1ad5dbc1100595cc0df1cdd6ad2
gsmereka <gsmereka200@gmail.com>
2025-09-09 02:18:55 +0000

    feat: add Person model and interface

---
+++ src/model/IPerson.groovy
@@ -0,0 +1,9 @@
+package model
+
+interface IPerson {
+    String getName()
+    String getEmail()
+    String getState()
+    String getCep()
+    String getDescription()
+}
---
+++ src/model/Person.groovy
@@ -0,0 +1,36 @@
+package model
+
+abstract class Person implements IPerson {
+    String name
+    String email
+    String state
+    String cep
+    String description
+    List<String> skills = []
+
+    Person(String name, String email, String state, String cep, String description, List<String> skills) {
+        this.name = name
+        this.email = email
+        this.state = state
+        this.cep = cep
+        this.description = description
+        if (skills) this.skills.addAll(skills)
+    }
+
+    @Override
+    String toString() {
+        String comp
+        if (skills != null && !skills.isEmpty()) {
+            comp = String.join(", ", skills)
+        } else {
+            comp = "—"
+        }
+
+        return "Nome: " + name + "\n" +
+                "E-mail: " + email + "\n" +
+                "Estado: " + state + "\n" +
+                "CEP: " + cep + "\n" +
+                "Descrição: " + description + "\n" +
+                "Competências: " + comp
+    }
+}
