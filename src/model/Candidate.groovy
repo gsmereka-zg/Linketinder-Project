3ba50d027e63478f2f3436c5225890b69c18cc8c
gsmereka <gsmereka200@gmail.com>
2025-09-09 02:24:57 +0000

    feat: add Candidate model

---
+++ src/model/Candidate.groovy
@@ -0,0 +1,19 @@
+package model
+
+class Candidate extends Person {
+    String cpf
+    Integer age
+
+    Candidate(String name, String email, String cpf, Integer age, String state, String cep, String description, List<String> skills) {
+        super(name, email, state, cep, description, skills)
+        this.cpf = cpf
+        this.age = age
+    }
+
+    @Override
+    String toString() {
+        return super.toString() +
+                "CPF: " + cpf + "\n" +
+                "Idade: " + age + "\n"
+    }
+}
